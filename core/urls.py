89c27b01c97ae863d2acfe1019ced1ee2dbe4ac3
Tanu Kumari <tanu424219@gmail.com>
2026-07-24 10:09:09 +0530

    add root redirect to admin

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('',lambda request: redirect('admin/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Hamari api app ko connect kiya
 ]
